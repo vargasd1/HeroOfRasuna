7802ded635232fc03a05e926399108840d0e187b
Justin Lai <laij3@ferris.edu>
2021-04-26 16:57:16 -0400

    Puzzle

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    //Attached to Door
+    public GameObject puzzle0, puzzle1, puzzle2;
+    Vector3 openPosition;
+    Vector3 closedPosition;
+    float time = 0f;
+
+    void Start()
+    {
+        closedPosition = transform.position;
+        openPosition = new Vector3(transform.position.x, transform.position.y + 10f, transform.position.z);
+    }
+
+    void FixedUpdate()
+    {
+        if (puzzle0.GetComponent<PuzzleSwitch>().correctPlace == true
+            && puzzle1.GetComponent<PuzzleSwitch>().correctPlace == true
+            && puzzle2.GetComponent<PuzzleSwitch>().correctPlace == true)
+        {
+            openDoor();
+            time += Time.fixedUnscaledDeltaTime;
+            puzzle0.GetComponent<PuzzleSwitch>().allowPuzzle = false;
+            puzzle1.GetComponent<PuzzleSwitch>().allowPuzzle = false;
+            puzzle2.GetComponent<PuzzleSwitch>().allowPuzzle = false;
+        }
+    }
+
+    void openDoor()
+    {
+        transform.position = Vector3.Lerp(closedPosition, openPosition, time / 2.5f);
+    }
+}
---
+++ Assets/Scripts/PuzzleSwitch.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSwitch : MonoBehaviour
+{
+    //Attached to Puzzle Platform > Switch 0/1/2
+
+    public GameObject player;
+    public GameObject puzzlePiece;
+    public float[] destinations = {-18f, 2f, 12f};
+    float currentX = 0f;
+    float y = 0f;
+    public float z = 0f;
+    float time = 0f;
+    int posCheck = 0;
+    int nextPosCheck = 1;
+    bool move = false;
+    public bool correctPlace = false;
+    public bool allowPuzzle = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G) && checkPlayerDistance() && allowPuzzle)
+        {
+            Debug.Log("Moving piece");
+            if (!move) move = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.G)) Debug.Log("Not close enough");
+    }
+
+    // Update is called once per physics update
+    void FixedUpdate()
+    {
+        if (move)
+        {
+            time += Time.fixedDeltaTime;
+            movePiece();
+            if(time > 3f)
+            {
+                move = false;
+                time = 0f;
+                ++posCheck;
+                ++nextPosCheck;
+                if (posCheck > destinations.Length -1) posCheck = 0;
+                if (nextPosCheck > destinations.Length - 1) nextPosCheck = 0;
+
+                if (posCheck == 1) correctPlace = true;
+                else correctPlace = false;
+            }
+        }
+    }
+
+    void movePiece()
+    {
+        currentX = Mathf.Lerp(destinations[posCheck], destinations[nextPosCheck], time / 2.5f);
+        puzzlePiece.transform.localPosition = new Vector3(currentX, y, z);
+    }
+
+    bool checkPlayerDistance()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) < 3f) return true;
+        else return false;
+    }
+}

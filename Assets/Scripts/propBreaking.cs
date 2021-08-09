1bcd936920e9a420a5d3b345b8baddceee37168f
David <davidvardgas@gmail.com>
2021-08-08 22:31:06 -0400

    Started Object Destruction
    +Added a lantern prefab and script that destroys it
    
    Note- the script currently checks for collision with the player, not the sword

---
+++ Assets/Scripts/propBreaking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propBreaking : MonoBehaviour
+{
+    public GameObject fractured;
+
+
+    public void OnTriggerEnter(Collider hit)
+    {
+        if(hit.tag == "Player"){
+            breakIt();
+        }
+    }
+
+    public void breakIt()
+    {
+        Instantiate(fractured, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+}

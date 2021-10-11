2f7eb07afa23c0081a56f69350490fbb80840b4a
Justin Lai <laij3@ferris.edu>
2021-10-11 12:57:45 -0400

    Puzzle adjustments with models

--- Assets/Scripts/PuzzleActive.cs
+++ Assets/Scripts/PuzzleActive.cs
@@ -5,61 +5,104 @@ using UnityEngine;
 public class PuzzleActive : MonoBehaviour
 {
     public GameObject discOuter, discMid, discInner, camPuzzle, highlightInner, highlightMid, highlightOuter;
+    float inRot, midRot, outRot, startIn, startMid, startOut, time = 0f;
     int selectedPiece = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool finishPuzzle = false;
 
     void Update()
     {
         //move the puzzle pieces in puzzle mode
         if (camPuzzle.activeSelf)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            //if puzzle is close enough to complete
+            if(Mathf.Abs(discInner.transform.rotation.z) <= 6f * (Mathf.PI / 180f) && Mathf.Abs(discMid.transform.rotation.z) <= 6f * (Mathf.PI / 180f) && Mathf.Abs(discOuter.transform.rotation.z) <= 6f * (Mathf.PI / 180f))
             {
-                ++selectedPiece;
-                if (selectedPiece > 2) selectedPiece = 2;
-                HighlightPuzzle();
+                finishPuzzle = true;
+                startIn = discInner.transform.rotation.z;
+                startMid = discMid.transform.rotation.z;
+                startOut = discOuter.transform.rotation.z;
             }
-            else if (Input.GetKeyDown(KeyCode.S))
+            //allow player to move puzzle
+            else
             {
-                --selectedPiece;
-                if (selectedPiece < 0) selectedPiece = 0;
-                HighlightPuzzle();
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                switch(selectedPiece)
+                if (Input.GetKeyDown(KeyCode.W))
                 {
-                    case 0:
-                        discInner.transform.Rotate(-2.0f, 0.0f, 0.0f, Space.Self);
-                        break;
-                    case 1:
-                        discMid.transform.Rotate(-2.0f, 0.0f, 0.0f, Space.Self);
-                        break;
-                    case 2:
-                        discOuter.transform.Rotate(-2.0f, 0.0f, 0.0f, Space.Self);
-                        break;
+                    ++selectedPiece;
+                    if (selectedPiece > 2) selectedPiece = 2;
+                    HighlightPuzzle();
                 }
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                switch (selectedPiece)
+                else if (Input.GetKeyDown(KeyCode.S))
                 {
-                    case 0:
-                        discInner.transform.Rotate(2.0f, 0.0f, 0.0f, Space.Self);
-                        break;
-                    case 1:
-                        discMid.transform.Rotate(2.0f, 0.0f, 0.0f, Space.Self);
-                        break;
-                    case 2:
-                        discOuter.transform.Rotate(2.0f, 0.0f, 0.0f, Space.Self);
-                        break;
+                    --selectedPiece;
+                    if (selectedPiece < 0) selectedPiece = 0;
+                    HighlightPuzzle();
                 }
-            }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    switch(selectedPiece)
+                    {
+                        case 0:
+                            discInner.transform.Rotate(0.0f, 0.0f, -2.0f, Space.Self);
+                            break;
+                        case 1:
+                            discMid.transform.Rotate(0.0f, 0.0f, -2.0f, Space.Self);
+                            break;
+                        case 2:
+                            discOuter.transform.Rotate(0.0f, 0.0f, -2.0f, Space.Self);
+                            break;
+                    }
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    switch (selectedPiece)
+                    {
+                        case 0:
+                            discInner.transform.Rotate(0.0f, 0.0f, 2.0f, Space.Self);
+                            break;
+                        case 1:
+                            discMid.transform.Rotate(0.0f, 0.0f, 2.0f, Space.Self);
+                            break;
+                        case 2:
+                            discOuter.transform.Rotate(0.0f, 0.0f, 2.0f, Space.Self);
+                            break;
+                    }
+                }
+            }//else allow player to move puzzle
+        }//if(camPuzzle.activeSelf)
+    }//Update()
+
+    void FixedUpdate()
+    {
+        if(finishPuzzle)
+        {
+            //adjust all rotations to be 0
+            if (time > 1f) time = 1f;
+            inRot = Mathf.Lerp(startIn, 0f, time);
+            midRot = Mathf.Lerp(startMid, 0f, time);
+            outRot = Mathf.Lerp(startOut, 0f, time);
+
+            discInner.transform.rotation = new Quaternion(0f, 0f, inRot, Quaternion.identity.w);
+            discMid.transform.rotation = new Quaternion(0f, 0f, midRot, Quaternion.identity.w);
+            discOuter.transform.rotation = new Quaternion(0f, 0f, outRot, Quaternion.identity.w);
+
+            time += Time.fixedDeltaTime;
+        }
+
+        if(time >= 6f)
+        {
+            //disable view for staircase being unlocked
+            //enable player
+            /*playerObj.SetActive(true);
+            canvasUI.SetActive(true);
+            camMain.SetActive(true);*/
+        }
+        else if(time >= 1f)
+        {
+            finishPuzzle = false;
+            //disable all puzzle parts
+            /*puzzleUI.SetActive(false);
+            camPuzzle.SetActive(false);*/
+            //enable view for staircase being unlocked in the second floor
         }
     }
 
